244f6d4aa6522bdee9d2b5d4c464b6d1a84193f6
borisprecijan99 <borisprecijanbp@gmail.com>
2021-06-19 22:58:37 +0200

    Added Eureka discovery server for microservice architecture

---
+++ backend/discovery-server/src/test/java/code9/project/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package code9.project.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
